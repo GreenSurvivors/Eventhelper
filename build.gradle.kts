b1d04f0593c53a2425f59ab0667caabecc48833b
FireInstall <feuerreiter@greensurvivors.de>
2024-09-24 04:39:34 +0200

    Set version to 2.0, since this might be a complete rewrite anyway. Auto download dependencies in run server task added expected values to tnt and inventory regions config.yaml Fix wrong expectancy of worlguard enabled state when loading the plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "de.greensurvivors"
-version = "1.3.0-SNAPSHOT"
+version = "2.0.0-SNAPSHOT"
 description = "Helper for all kinds of Events."
 // this is the minecraft major version. If you need a subversion like 1.20.1,
 // change it in the dependencies section as this is also used as the api version of the plugin.yml
@@ -17,8 +17,8 @@ val mainMCVersion by extra("1.20")
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
@@ -42,22 +42,35 @@ dependencies {
 }
 
 tasks {
-  // Configure reobfJar to run when invoking the build task
-  assemble {
-    dependsOn(reobfJar)
-  }
+    // Configure reobfJar to run when invoking the build task
+    assemble {
+        dependsOn(reobfJar)
+    }
 
-  compileJava {
-    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
 
-    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-    // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(17)
-  }
-  
-  processResources {
-      filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
 
-      expand(project.properties)
-  }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        expand(project.properties)
+    }
+
+
+    runServer {
+        downloadPlugins {
+            // make sure to double check the version id on the Modrinth version page
+            // for 1.21+ :
+            //modrinth("worldguard", "J66QOTLZ")
+            // for 1.20.4 --> 1.21
+            url("https://mediafilez.forgecdn.net/files/4675/318/worldguard-bukkit-7.0.9-dist.jar")
+            // we have to ignore all newer versions since they depend on java 21
+            modrinth("worldedit", "JzCMkGax")
+        }
+    }
 }
