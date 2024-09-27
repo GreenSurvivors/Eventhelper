74ad4f61a2f90f2425684932605b0a608c1d21d4
FireInstall <feuerreiter@greensurvivors.de>
2024-09-27 05:29:15 +0200

    added dependency to simple quests to set up tasks (todo) added player types (alive, perished, spectator)

--- src/main/java/de/greensurvivors/eventhelper/DependencyManager.java
+++ src/main/java/de/greensurvivors/eventhelper/DependencyManager.java
@@ -8,11 +8,13 @@ import org.jetbrains.annotations.Nullable;
 public class DependencyManager {
     private final @NotNull EventHelper plugin;
     private final @Nullable Plugin worldGuard;
+    private final @Nullable Plugin simpleQuests;
 
     public DependencyManager(final @NotNull EventHelper plugin) {
         this.plugin = plugin;
 
         this.worldGuard = Bukkit.getPluginManager().getPlugin("WorldGuard");
+        this.simpleQuests = Bukkit.getPluginManager().getPlugin("SimpleQuests");
     }
 
     public boolean isWorldGuardEnabled() {
@@ -21,5 +23,9 @@ public class DependencyManager {
 
     public boolean isWorldGuardInstanceSafe() {
         return worldGuard != null;
+    }
+
+    public boolean isSimpleQuestsEnabled() {
+        return simpleQuests != null && simpleQuests.isEnabled();
     }
 }
