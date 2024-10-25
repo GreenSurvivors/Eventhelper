fdfd492edf95ce663ab6b5503555671d536f35c7
FireInstall <feuerreiter@greensurvivors.de>
2024-10-25 15:38:50 +0200

    Added warden like api to the vexes

--- src/main/java/de/greensurvivors/eventhelper/modules/ghost/vex/IVex.java
+++ src/main/java/de/greensurvivors/eventhelper/modules/ghost/vex/IVex.java
@@ -3,7 +3,9 @@ package de.greensurvivors.eventhelper.modules.ghost.vex;
 import de.greensurvivors.eventhelper.modules.ghost.GhostGame;
 import org.bukkit.Location;
 import org.bukkit.entity.Enemy;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Flying;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -16,5 +18,103 @@ public interface IVex extends Flying, Enemy {
                          final @NotNull GhostGame ghostGame,
                          final @Nullable Consumer<IVex> function) {
         return VexCraftEntity.spawnNew(location, reason, ghostGame, function);
+    }
+
+    /**
+     * Gets the anger level of this warden.
+     * <p>
+     * Anger is an integer from 0 to 150. Once a Warden reaches 80 anger at a
+     * target it will actively pursue it.
+     *
+     * @return anger level
+     */
+    int getAnger();
+
+    /**
+     * Gets the anger level of this warden.
+     * <p>
+     * Anger is an integer from 0 to 150. Once a Warden reaches 80 anger at a
+     * target it will actively pursue it.
+     *
+     * @param entity target entity
+     * @return anger level
+     */
+    int getAnger(@NotNull Entity entity);
+
+    /**
+     * Gets the highest anger level of this warden.
+     * <p>
+     * Anger is an integer from 0 to 150. Once a Warden reaches 80 anger at a
+     * target it will actively pursue it.
+     *
+     * @return highest anger level
+     */
+    int getHighestAnger();
+
+    /**
+     * Increases the anger level of this warden.
+     * <p>
+     * Anger is an integer from 0 to 150. Once a Warden reaches 80 anger at a
+     * target it will actively pursue it.
+     *
+     * @param entity   target entity
+     * @param increase number to increase by
+     * @see #getAnger(org.bukkit.entity.Entity)
+     */
+    void increaseAnger(@NotNull Entity entity, int increase);
+
+    /**
+     * Sets the anger level of this warden.
+     * <p>
+     * Anger is an integer from 0 to 150. Once a Warden reaches 80 anger at a
+     * target it will actively pursue it.
+     *
+     * @param entity target entity
+     * @param anger  new anger level
+     * @see #getAnger(org.bukkit.entity.Entity)
+     */
+    void setAnger(@NotNull Entity entity, int anger);
+
+    /**
+     * Clears the anger level of this warden.
+     *
+     * @param entity target entity
+     */
+    void clearAnger(@NotNull Entity entity);
+
+    /**
+     * Gets the {@link LivingEntity} at which this warden is most angry.
+     *
+     * @return The target {@link LivingEntity} or null
+     */
+    @Nullable LivingEntity getEntityAngryAt();
+
+    /**
+     * Make the warden sense a disturbance in the force at the location given.
+     *
+     * @param location location of the disturbance
+     */
+    void setDisturbanceLocation(@NotNull Location location);
+
+    /**
+     * Get the level of anger of this warden.
+     *
+     * @return The level of anger
+     */
+    @NotNull AngerLevel getAngerLevel();
+
+    public enum AngerLevel {
+        /**
+         * Anger level 0-39.
+         */
+        CALM,
+        /**
+         * Anger level 40-79.
+         */
+        AGITATED,
+        /**
+         * Anger level 80 or above.
+         */
+        ANGRY;
     }
 }
--- src/main/java/de/greensurvivors/eventhelper/modules/ghost/vex/VexCraftEntity.java
+++ src/main/java/de/greensurvivors/eventhelper/modules/ghost/vex/VexCraftEntity.java
@@ -1,13 +1,17 @@
 package de.greensurvivors.eventhelper.modules.ghost.vex;
 
 import de.greensurvivors.eventhelper.modules.ghost.GhostGame;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_20_R3.CraftServer;
 import org.bukkit.craftbukkit.v1_20_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_20_R3.entity.CraftEnemy;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftEntity;
 import org.bukkit.craftbukkit.v1_20_R3.entity.CraftMob;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -43,5 +47,57 @@ public class VexCraftEntity extends CraftMob implements IVex, CraftEnemy {
     @Override
     public @NotNull String toString() {
         return "CraftGhostVex";
+    }
+
+    @Override
+    public int getAnger() {
+        return this.getHandle().getAngerManagement().getActiveAnger(this.getHandle().getTarget());
+    }
+
+    @Override
+    public int getAnger(@NotNull Entity entity) {
+        return this.getHandle().getAngerManagement().getActiveAnger(((CraftEntity) entity).getHandle());
+    }
+
+    // Paper start
+    @Override
+    public int getHighestAnger() {
+        return this.getHandle().getAngerManagement().getActiveAnger(null);
+    }
+    // Paper end
+
+    @Override
+    public void increaseAnger(@NotNull Entity entity, int increase) {
+        this.getHandle().getAngerManagement().increaseAnger(((CraftEntity) entity).getHandle(), increase);
+    }
+
+    @Override
+    public void setAnger(@NotNull Entity entity, int anger) {
+        this.getHandle().clearAnger(((CraftEntity) entity).getHandle());
+        this.getHandle().getAngerManagement().increaseAnger(((CraftEntity) entity).getHandle(), anger);
+    }
+
+    @Override
+    public void clearAnger(@NotNull Entity entity) {
+        this.getHandle().clearAnger(((CraftEntity) entity).getHandle());
+    }
+
+    @Override
+    public LivingEntity getEntityAngryAt() {
+        return (LivingEntity) this.getHandle().getEntityAngryAt().map(net.minecraft.world.entity.Entity::getBukkitEntity).orElse(null);
+    }
+
+    @Override
+    public void setDisturbanceLocation(@NotNull Location location) {
+        VexAI.setDisturbanceLocation(this.getHandle(), BlockPos.containing(location.getX(), location.getY(), location.getZ()));
+    }
+
+    @Override
+    public @NotNull AngerLevel getAngerLevel() {
+        return switch (this.getHandle().getAngerLevel()) {
+            case CALM -> AngerLevel.CALM;
+            case AGITATED -> AngerLevel.AGITATED;
+            case ANGRY -> AngerLevel.ANGRY;
+        };
     }
 }
