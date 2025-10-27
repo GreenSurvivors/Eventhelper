11cb38e9cc081aec7fba10ad0c0542567c3c2178
FireInstall <feuerreiter@greensurvivors.de>
2025-10-27 17:21:34 +0100

    use maven central mirror update gradle update dependencies move versioning to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,32 @@
 plugins {
     `java-library`
     //java
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
-    id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("xyz.jpenilla.run-paper") version "3.0.2" // Adds runServer and runMojangMappedServer tasks for testing
+    id("net.raphimc.class-token-replacer") version "1.1.7" // allows to replace strings in classes
 }
 
 group = "de.greensurvivors"
-version = "2.0.3-SNAPSHOT"
+version = buildString {
+    append(project.properties["pluginVersion"])
+
+    if ((project.properties["release"] as String).toBoolean().not()) {
+        append("-Snapshot")
+    }
+
+    append("+${project.properties["mcVersion"]}")
+}
+
 description = "Helper for all kinds of Events."
-val mcVersion by extra("1.21.4")
 
 // we only work with paper and downstream!
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 17 installed for example.
+    // If you need to compile to for example JVM 8 or 17 bytecode, adjust the 'release' option below and keep the toolchain at 21.
+    toolchain.languageVersion = JavaLanguageVersion.of("${rootProject.properties["javaVersion"]}")
+    sourceCompatibility = JavaVersion.toVersion(rootProject.properties["javaVersion"]!!)
 }
 
 repositories {
@@ -33,11 +44,30 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("$mcVersion-R0.1-SNAPSHOT")
-    compileOnly("com.sk89q.worldguard", "worldguard-bukkit", "7.0.14-SNAPSHOT") //newest worldguard version
-    api("com.github.ben-manes.caffeine", "caffeine", "3.2.2") // caches
-    compileOnly("de.greensurvivors", "SimpleQuests", "2.0.0") // installed locally as we depend on a dev version
-    api("org.apache.commons", "commons-collections4", "4.5.0")
+    paperweight.paperDevBundle("${project.properties["mcVersion"]}-R0.1-SNAPSHOT")
+    compileOnly(
+        "com.sk89q.worldguard",
+        "worldguard-bukkit",
+        "${project.properties["worldguardVersion"]}"
+    ) //newest worldguard version
+    api("com.github.ben-manes.caffeine", "caffeine", "${project.properties["caffeineVersion"]}") // caches
+    compileOnly(
+        "de.greensurvivors",
+        "SimpleQuests",
+        "${project.properties["simpleQuestsVersion"]}"
+    ) // installed locally as we depend on a dev version
+    api("org.apache.commons", "commons-collections4", "${project.properties["commons-collections4Version"]}")
+}
+
+// set up the versions to get replaced in the Loader class
+sourceSets {
+    main {
+        classTokenReplacer {
+            property($$"${version}", version)
+            property($$"${caffeineVersion}", "${project.properties["caffeineVersion"]}")
+            property($$"${commons-collections4Version}", "${project.properties["commons-collections4Version"]}")
+        }
+    }
 }
 
 tasks {
@@ -59,8 +89,8 @@ tasks {
     runServer {
         downloadPlugins {
             // make sure to double-check the version id on the Modrinth version page
-            modrinth("worldguard", "f9NoeotB")
-            modrinth("worldedit", "Jk1z2u7n")
+            modrinth("worldedit", "${project.properties["worldeditVersionRunTask"]}")
+            modrinth("worldguard", "${project.properties["worldguardVersionRunTask"]}")
         }
     }
 }
--- src/main/java/de/greensurvivors/eventhelper/Loader.java
+++ src/main/java/de/greensurvivors/eventhelper/Loader.java
@@ -14,11 +14,11 @@ public class Loader implements PluginLoader {
     public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
 
-        resolver.addRepository(new RemoteRepository.Builder("maven central", "default", "https://repo1.maven.org/maven2/").build());
+        resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
 
-        DefaultArtifact caffeineArtifact = new DefaultArtifact("com.github.ben-manes.caffeine:caffeine:3.1.8");
+        DefaultArtifact caffeineArtifact = new DefaultArtifact("com.github.ben-manes.caffeine:caffeine:${caffeineVersion}");
         resolver.addDependency(new Dependency(caffeineArtifact, null));
-        DefaultArtifact collectionsArtifact = new DefaultArtifact("org.apache.commons:commons-collections4:4.5.0-M2");
+        DefaultArtifact collectionsArtifact = new DefaultArtifact("org.apache.commons:commons-collections4:${commons-collections4Version}");
         resolver.addDependency(new Dependency(collectionsArtifact, null));
 
         classpathBuilder.getContext().getLogger().info("loaded libraries {}, version: {} and {}, version: {}",
